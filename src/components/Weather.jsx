c42f4b3309b85ef06b02084a9c2c71ccda057e9b
NiladriBlue <niladriroy9163@gmail.com>
2024-09-13 14:47:14 +0530

    weather

---
+++ src/components/Weather.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
+import cloud_icon from "../assets/cloud.png";
+import dizzle_icon from "../assets/dizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.jpeg";
+
+const Weather = () => {
+
+    const [weatherdata, setweatherdata] = useState(false); 
+
+    const allIcons= {
+      "01d": clear_icon,
+      "01n": clear_icon,
+      "02d": cloud_icon,
+      "02n": cloud_icon,
+      "03d": cloud_icon,
+      "03n": cloud_icon,
+      "04d": dizzle_icon,
+      "04n": dizzle_icon,
+      "09d": rain_icon,
+      "09n": rain_icon,
+      "10d": rain_icon,
+      "10n": rain_icon,
+      "13d": snow_icon,
+      "13n": snow_icon,
+    }
+
+  const search= async(city)=>{
+    try{
+      const url='https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${VITE_APP_ID}';
+  
+const response = await fetch(url);
+const data = await response.json();
+console.log(data);
+const icon = allIcons[data.Weather[0].icon] || clear_icon;
+setweatherdata({
+  humidity: data.main.humidity,
+  windspeed: data.wind.speed,
+  temparature: Math.floor(data.main.temp),
+  location: data.name,
+  icon: icon
+})
+
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(()=>{
+    search("London");
+  },[])
+
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type='text' placeholder='search'/>
+            <img src={search_icon} alt="" />
+        </div>
+      <img src={weatherdata.icon } alt="" className='weather-icon'/>
+      <p className='temperature'>{weatherdata.temparature}°C</p>
+      <p className='location'>{weatherdata.location}</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>{weatherdata.humidity} %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>{weatherdata.windspeed} Km/h</p>
+            <span>Wind speed</span> 
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
